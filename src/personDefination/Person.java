733240a6e2845e7e68be11d50077729e31dbd823
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-02-22 19:30:55 +0530

    creating fields of person

--- src/personDefination/Person.java
+++ src/personDefination/Person.java
@@ -1,5 +1,12 @@
 package personDefination;
 
+import java.util.ArrayList;
+
 public class Person {
+    ArrayList<Long> numberList = new ArrayList<>();
+    private String firstName;
+    private String lastName;
+    private String email;
+
 
 }
