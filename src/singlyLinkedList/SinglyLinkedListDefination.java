2c5982820dd99081b298cc6b6f8d4fadab78ed8e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-02-22 18:56:24 +0530

    Node class created

--- src/singlyLinkedList/SinglyLinkedListDefination.java
+++ src/singlyLinkedList/SinglyLinkedListDefination.java
@@ -4,6 +4,7 @@ import ADT.LinkedListADT;
 
 public class SinglyLinkedListDefination<E> implements LinkedListADT<E> {
 
+
     @Override
     public void add(E item) {
 
@@ -28,4 +29,26 @@ public class SinglyLinkedListDefination<E> implements LinkedListADT<E> {
     public void print() {
 
     }
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        private Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        private Node(E data) {
+            this.data = data;
+        }
+
+        private E getData() {
+            return data;
+        }
+
+        private Node<E> getNext() {
+            return next;
+        }
+    }
+
 }
