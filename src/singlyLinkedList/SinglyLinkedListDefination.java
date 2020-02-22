52748ec2bf28a5cd43bd5a8cd8ea9f9df087edc7
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2020-02-22 18:47:56 +0530

    implement linkedList adt on SinglyLinkedListDefination

--- src/singlyLinkedList/SinglyLinkedListDefination.java
+++ src/singlyLinkedList/SinglyLinkedListDefination.java
@@ -1,4 +1,31 @@
 package singlyLinkedList;
 
-public class SinglyLinkedListDefination {
+import ADT.LinkedListADT;
+
+public class SinglyLinkedListDefination<E> implements LinkedListADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove(E item) {
+        return null;
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
